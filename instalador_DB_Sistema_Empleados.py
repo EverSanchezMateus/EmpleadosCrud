d40c955a3753aa257d82b4413ba28a5fbfa9b2f3
EverSanchezMateus <eversanchezmateus@gmail.com>
2024-09-27 11:59:08 -0500

    agregue funcion agregarEmpleado

--- instalador_DB_Sistema_Empleados.py
+++ instalador_DB_Sistema_Empleados.py
@@ -36,7 +36,7 @@ def crear_base_de_datos():
             email VARCHAR(255),
             telefono BIGINT,
             foto LONGBLOB,
-            estado TINYINT(1) NOT NULL DEFAULT 1
+            estado TINYINT(1) NOT NULL
                 
         )''')
 
