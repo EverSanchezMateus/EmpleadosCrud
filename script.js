11a79e76c05304635eb6dd74e4ac618f2ad27168
EverSanchezMateus <144500586+EverSanchezMateus@users.noreply.github.com>
2024-10-07 12:07:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,47 @@ function closeModal(modalId) {
 }
 
 
-function agregarEmpleado(){
-    let nombre =document.getElementById("addName").value
-    let apellido= document.getElementById("addSurName").value
-    let documento= document.getElementById("addDocument").value
+function agregarEmpleado(event) {
+    event.preventDefault();
+
+    let nombre = document.getElementById("addName").value;
+    let apellido = document.getElementById("addSurName").value;
+    let documento_identidad = document.getElementById("addDocument").value;
+    let direccion = document.getElementById("addAddress").value;
+    let email = document.getElementById("addEmail").value;
+    let celular = document.getElementById("addTel").value;
+    let fotoElement = document.getElementById("addFoto");
+  
+    foto = fotoElement.files[0];
+
+
+    let estado = document.getElementById("addStatus").value;
+
+    let datosFormulario = new FormData();
+    datosFormulario.append("nombre", nombre);
+    datosFormulario.append("apellido", apellido);
+    datosFormulario.append("documento_identidad", documento_identidad);
+    datosFormulario.append("direccion", direccion);
+    datosFormulario.append("email", email);
+    datosFormulario.append("telefono", celular);
+    if (foto) {
+        datosFormulario.append("foto", foto);
+    }
+  
+    datosFormulario.append("estado", estado);
+
+    fetch('agregarEmpleado.php', { 
+        method: 'POST',
+        body: datosFormulario
+    })
+    .then(response => response.text())
+    .then(data => {
+        //document.querySelector('#addEmployeeModal form').reset(); // Limpiar el formulario después de agregar el empleado por si se agrega otro
+        //cerrarModal('addEmployeeModal');
+        alert(data)
+    })
     
-    
-    
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
